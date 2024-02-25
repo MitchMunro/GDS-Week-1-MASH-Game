125148a0445abb0f9d2e8c22d459fc2041d380b7
MitchMunro <mitchmunro@gmail.com>
2024-02-25 13:49:30 +1100

    Program People rescue complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    public int PeopleOnBoat { get; private set; } = 0;
-    [SerializeField] private int PeopleRescued = 0;
+    public static GameManager Inst;
+    public int PeopleOnBoat = 0; //{ get; private set; } = 0;
+    public int PeopleRescued = 0;
+
+    private void Awake()
+    {
+        //Set up GameController as a Singleton
+        if (Inst == null)
+        {
+            Inst = this;
+        }
+        else
+        {
+            Debug.Log("Multiple Game controllers in scene. Deleting this one.");
+            Destroy(this);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +35,22 @@ public class GameController : MonoBehaviour
         PeopleOnBoat = 0;
     }
 
-    public void PickUpPerson()
+    /// <summary>
+    /// Returns true if the person can be picked up. Returns false if otherwise.
+    /// </summary>
+    /// <returns></returns>
+    public bool PickUpPerson()
     {
-        if(PeopleOnBoat < 3) PeopleOnBoat++;
+        if (PeopleOnBoat < 3)
+        {
+            PeopleOnBoat++;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/PersonLogic.cs
+++ Assets/Scripts/PersonLogic.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Person : MonoBehaviour
+public class PersonLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameManager GM;
+
+    private void Start()
     {
-        
+        GM = GameManager.Inst;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "PlayerBoat")
+        {
+            Debug.Log("Collided with boat.");
+
+            if (GM.PickUpPerson())
+            {
+                Debug.Log("Pick up person passed.");
+
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Debug.Log("Maximum passengers reached.");
+            }
+        }
     }
 }
